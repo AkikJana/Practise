54bfb10e642ada326b092ca8dc5797b6c536043e
AkikJana <akik.e.aj@gmail.com>
2018-11-06 21:14:26 +0530

    Made Some changes

--- Project1/app1.py
+++ Project1/app1.py
@@ -2,7 +2,13 @@ import json
 data=json.load(open("data.json"))
 
 def translate(w):
-	return data[w]
+	w=w.lower()
+	if w in data:
+
+		return data[w]
+	else:
+		return "the word does not exist."
+
 
 word=input("enter word: ")
-print(translate(word))
+print(translate(word))  
